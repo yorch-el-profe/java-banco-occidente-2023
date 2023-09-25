a1f6d8894f4c192489ffef51f331f4266a7c6d90
Jorge Ramón <jorge.ramon2128@gmail.com>
2023-09-25 08:15:15 -0600

    Sesion 01: Tipos de datos

---
+++ sesion-01-introduccion/02-tipos-datos/TipoDatos.java
@@ -0,0 +1,38 @@
+public class TipoDatos {
+
+  public static void main(String... args) {
+    // Para crear una variable se hace de la siguiente forma:
+    // tipo nombrevariable = valor;
+
+    // Tipos de datos PRIMITIVOS
+
+    // byte almacena 1 byte (-128 a 127)
+    byte miPrimerByte = 127;
+
+    // short almacena 2 bytes (-32,768 a 32,767)
+    short miPrimerShort = 30000;
+
+    // Enteros (int)
+    int edad = 20;
+
+    // Enteros grandes (long)
+    long poblacionMundial = 788800000;
+
+    // Tipos datos de punto flotante
+    float pi = 3.1416f;
+
+    double random = 43.7239712938719;
+
+    // Boolean
+    boolean esVerdad = true;
+
+    // Caracteres
+    char letraA = 'A';
+
+    // Cadenas de texto (String) (no es un tipo de dato primitivo)
+    String texto = "Hola Mundo";
+
+    // Java infiere el tipo de dato
+    var x = 0;
+  }
+}
