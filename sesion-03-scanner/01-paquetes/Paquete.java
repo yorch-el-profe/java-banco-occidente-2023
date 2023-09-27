0e7af00d3f292956309cae972776ee25c23a273b
Jorge Ramón <jorge.ramon2128@gmail.com>
2023-09-27 07:22:43 -0600

    Sesion 03: Comentario sobre java.lang

--- sesion-03-scanner/01-paquetes/Paquete.java
+++ sesion-03-scanner/01-paquetes/Paquete.java
@@ -1,3 +1,5 @@
+// Las clases del paquete java.lang ya están cargados por default.
+
 // Importar TODAS las clases del paquete especificado
 // Recomendación: No usar este tipo de importación
 // import java.util.*;
